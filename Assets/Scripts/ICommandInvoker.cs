d08416ae73f60e55f7fe44cf4eea915726a77f90
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-08-09 22:19:43 +0900

    ICommandInvoker 올림
    이 인터페이스에 따라 CommandManager를 구현하면 됨.

---
+++ Assets/Scripts/ICommandInvoker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+interface ICommandInvoker
+{
+    void Conjure(EntityType type);
+    void Change(EntityType type);
+
+    bool IsConjurable(EntityType type);
+    bool IsChangable(EntityType type);
+}
