0196d7541384b5fc290c4bcf4dbb39c0c2a1f21c
Davin <bdv951101@gmail.com>
2018-08-12 20:37:37 +0900

    튜토리얼 스테이지에서 Help 팝업이 뜨도록 함
    Help_Trigger로서 임시로 하얀 구를 사용. 적절한 위치에 6개 배치.

---
+++ Assets/Scripts/HelpManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelpManager : MonoBehaviour {
+
+    public GameObject Help;
+	
+	private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            Help.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Help.SetActive(false);
+        }
+    }
+}
