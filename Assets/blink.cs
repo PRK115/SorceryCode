27a387018f26fca67cd47710f1cee01ee31b5365
Unknown <345t@naver.com>
2018-08-28 11:45:16 +0900

    인트로/스테이지 선택 씬 아트 작업

---
+++ Assets/blink.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class blink : MonoBehaviour {
+
+    bool black = true;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ColorChange());
+	}
+
+    IEnumerator ColorChange()
+    {
+        while (true)
+        {
+            if (black)
+            {
+                gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+                black = false;
+            }
+            else
+            {
+                gameObject.GetComponent<Image>().color = new Color(0, 0, 0, 255);
+                black = true;
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
