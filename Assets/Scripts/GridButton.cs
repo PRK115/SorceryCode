df2692e7ceecf708061f207b8e5259ff0d535b07
LAPTOP-CGBA8N6T\345t <345t@naver.com>
2019-03-05 14:44:27 +0900

    그리드 기능 추가

---
+++ Assets/Scripts/GridButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridButton : MonoBehaviour {
+
+    public GameObject gridPrefab;
+    private GameObject grid;
+
+    private void Awake()
+    {
+        grid = Instantiate(gridPrefab);
+        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(GridOnOff);
+    }
+
+    public void GridOnOff()
+    {
+        if(grid != null)
+            grid.SetActive(!grid.activeInHierarchy);
+    }
+}
